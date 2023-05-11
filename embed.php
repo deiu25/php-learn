7825b66299349fc78b8568bd9bb090cad142b1db
deiu25 <d3iu25@gmail.com>
2023-05-11 23:37:34 +0300

    variabile, constante

--- embed.php
+++ embed.php
@@ -12,8 +12,23 @@
 
     <?php
 
-    $title = "Andrei First Page";
-
+//    Variabile:
+//    În PHP, variabilele încep cu un simbol $ urmat de numele variabilei.
+//    Numele unei variabile trebuie să înceapă cu o literă sau cu un underscore (_).
+    $title = "Variables:";
+    $name = 'Deiu';
+    $varsta = 30;
+    echo $name . " " . $varsta;
+    
+//    Constante:
+//    Constantele sunt valori care nu se pot modifica pe parcursul executării scriptului.
+//    Pentru a defini o constantă, se utilizează funcția define() sau cuvântul cheie const într-o clasă.
+    define("PI", 3.141592);
+    //sau intr-o clasa
+    class Cerc {
+        const PI = 3.141592;
+    }
+    echo PI;
     ?>
 
     <h1><?php echo $title; ?></h1>
