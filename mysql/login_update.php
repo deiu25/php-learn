771f58421fdd39467472d57b032a2f8522055731
deiu25 <d3iu25@gmail.com>
2023-05-14 17:35:34 +0300

    update records

--- mysql/login_update.php
+++ mysql/login_update.php
@@ -1,8 +1,16 @@
-<?php include "db.php"; ?>
+<?php include "db.php";
+?>
 
-<?php include "functions.php"; ?>
+<?php include "functions.php";
+?>
 
+<?php
 
+if ( isset( $_POST['submit'] ) ) { 
+UpdateTable();
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,10 +25,14 @@
 <body>
     <div class="container">
         <div class="col-sm-6">
-            <form action="login_create.php" method="post">
+            <form action="login_update.php" method="post">
                 <div class="form-group">
                     <label for="username">Username</label>
                     <input type="text" name="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" class="form-control">
                 </div>
                 <div class="form-group">
                     <label for="password">Password</label>
@@ -28,9 +40,9 @@
                 </div>
                 <div class="form-group">
                     <select name="id" id="">
-                        <?php 
-                        showAllData();
-                        ?>
+                        <?php
+showAllData();
+?>
                     </select>
                 </div>
                 <input class="btn btn-primary" type="submit" name="submit" value="Update">
