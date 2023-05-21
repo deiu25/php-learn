8765e7946dd552d8c615c23ea8760969df9a60fb
deiu25 <d3iu25@gmail.com>
2023-05-21 19:35:20 +0300

    class methods, props

--- Learn/poo.php
+++ Learn/poo.php
@@ -1,10 +1,27 @@
 <?php
 
 class Car {
-    function MoveWheels(){
-        echo "Wheels move";
+    
+    var $wheels = 4;
+    var $hood = 1;
+    var $engine = "2.0";
+    var $doors = 5;
+    
+    function ChangeDoorsProp(){
+        $this -> doors = 3;
     }
 }
+
+$bmw = new Car();
+$dacia = new Car();
+
+$bmw -> ChangeDoorsProp();
+echo $bmw -> doors . " doors";   echo "<br><br>";
+
+$bmw -> engine = "3.0";
+echo $bmw -> engine . " engine";                echo "<br><br>";
+
+
 
 if(method_exists("Car", "MoveWheels")) {
     echo "The method exist";
